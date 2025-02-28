6705ab726101e50a0c6599922fe09b61bbc58b5e
xaratustrah <xaratustrah@users.noreply.github.com>
2025-02-28 21:13:11 +0100

    added rest sender

--- detectomer/zmqreceiver.py
+++ detectomer/zmqreceiver.py
@@ -1,6 +1,8 @@
 import zmq
 import numpy as np
 from pyqtgraph.Qt import QtCore, QtWidgets
+import requests
+from requests.exceptions import HTTPError
 from .mainwindow_ui import MainWindowUI
 
 class ZMQReceiver(MainWindowUI):
@@ -14,6 +16,8 @@ class ZMQReceiver(MainWindowUI):
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_plot)
+        
+        self.rest_message_sent = False
         
 
     def start_receiving(self):
@@ -51,6 +55,8 @@ class ZMQReceiver(MainWindowUI):
             
             if self.inverse_checkbox.isChecked():
                 fft_data = -fft_data - int(self.ref_value_spinbox.value())
+            else:
+                fft_data += int(self.ref_value_spinbox.value())
                 
             self.graph_widget.plot(self.freqs[:int(self.data_lframe / 2)], fft_data[:int(self.data_lframe / 2)], pen='w', clear=True)
             self.graph_widget.addItem(self.red_line)
@@ -74,26 +80,18 @@ class ZMQReceiver(MainWindowUI):
             self.graph_max_label.setText(f'Graph Max: {graph_max:.2f} dBm')
 
             if graph_max > self.vslider.value() and not self.inverse_checkbox.isChecked():
-                self.statusBar().setStyleSheet("background-color: red; color: white; font-weight: bold;")
-                self.statusBar().showMessage("Threshold crossed, no message sent.")
-                self.writeLog()
-
                 if self.rest_checkbox.isChecked():
-                    self.statusBar().setStyleSheet("background-color: purple; color: yellow; font-weight: bold;")
-                    self.statusBar().showMessage("Threshold crossed, sending REST message!")
+                    self.toggle_rest_message()
+                else:
+                    self.only_show_message()
+                    self.writeLog()
 
-            elif graph_min < self.vslider.value() and self.inverse_checkbox.isChecked():
-                self.statusBar().setStyleSheet("background-color: red; color: white; font-weight: bold;")
-                self.statusBar().showMessage("Threshold crossed, no message sent.")
-                self.writeLog()
-
+            if graph_min < self.vslider.value() and self.inverse_checkbox.isChecked():
                 if self.rest_checkbox.isChecked():
-                    self.statusBar().setStyleSheet("background-color: purple; color: yellow; font-weight: bold;")
-                    self.statusBar().showMessage("Threshold crossed, sending REST message!")
-
-            else:
-                self.statusBar().setStyleSheet("")
-                self.statusBar().showMessage("Ready")
+                    self.toggle_rest_message()
+                else:
+                    self.only_show_message()
+                    self.writeLog()
 
         except zmq.Again:
             pass
@@ -101,3 +99,51 @@ class ZMQReceiver(MainWindowUI):
             pass
         except AttributeError:
             QtWidgets.QMessageBox.warning(self, "Error", "Please enter ZMQ address and port")
+
+    def toggle_rest_message(self):
+        if self.rest_message_sent:
+            self.actually_send_rest_message(False)
+            self.rest_message_sent = False
+        else:
+            QtCore.QTimer.singleShot(self.rest_hold_time_spinbox.value()*1000, self.toggle_rest_message)
+            self.rest_message_sent = True
+            self.actually_send_rest_message(True)
+    
+    def only_show_message(self):
+        self.statusBar().setStyleSheet("background-color: red; color: white; font-weight: bold;")
+        self.statusBar().showMessage("Threshold crossed, no message sent.")
+        QtCore.QTimer.singleShot(300, self.clear_up_statusbar)
+    
+    def clear_up_statusbar(self):
+            self.statusBar().setStyleSheet("")
+            self.statusBar().clearMessage()
+        
+    def actually_send_rest_message(self, status):
+        if status:
+            self.statusBar().setStyleSheet("background-color: purple; color: yellow; font-weight: bold;")
+            self.statusBar().showMessage(f"Threshold crossed, holding REST message for {self.rest_hold_time_spinbox.value()} [s]")
+            data = {
+                "dynamicSignals": [
+                    {
+                        "enabled": status,
+                        "id": self.rest_scid
+                    }
+                ],
+                "staticSignals": []
+            }
+            headers = {
+                "Content-Type": "application/json"
+            }
+            try:
+                response = requests.put(self.rest_url, json=data, headers=headers)
+                response.raise_for_status()  # Check for HTTP errors
+                print(response.json())
+        
+            except HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err}")
+        
+            except Exception as err:
+                print(f"Other error occurred: {err}")
+        
+        else:
+            self.clear_up_statusbar()
